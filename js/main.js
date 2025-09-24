4b6ea70cb2ac90cb14170e3004718af0fcd2e242
beatricefini <finibeatrice@gmail.com>
2025-09-24 20:09:58 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,15 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log("🚀 main.js avviato");
 
   const sceneEl = document.querySelector('a-scene');
+  const container = document.getElementById('modelsContainer');
+
   sceneEl.addEventListener("arReady", () => {
     console.log("✅ AR pronta");
   });
 
-  sceneEl.addEventListener("targetFound", (e) => {
-    console.log("📍 targetFound EVENT:", e);
+  sceneEl.addEventListener("targetFound", () => {
+    console.log("📍 targetFound EVENT");
+
+    // Test: carichiamo un modello
+    const modelEl = document.createElement('a-entity');
+    modelEl.setAttribute('gltf-model', '#piece1'); // riferimento da <a-assets>
+    modelEl.setAttribute('scale', '0.2 0.2 0.2');
+    modelEl.setAttribute('position', '0 0 0');
+
+    container.appendChild(modelEl);
+
+    console.log("✅ Modello aggiunto a container");
   });
 
-  sceneEl.addEventListener("targetLost", (e) => {
-    console.log("👋 targetLost EVENT:", e);
+  sceneEl.addEventListener("targetLost", () => {
+    console.log("👋 targetLost EVENT");
   });
 });
